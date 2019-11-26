6bdaa2c39ae07443e718fd817834a97f62b08ee4
KornelijaK <46718930+KornelijaK@users.noreply.github.com>
2019-11-26 14:45:08 +0100

    Update 2_SELECT_FROM_WORLD.sql

--- 2_SELECT_FROM_WORLD.sql
+++ 2_SELECT_FROM_WORLD.sql
@@ -40,7 +40,11 @@ SELECT name, population, area
 FROM world
 WHERE (area > 3000000 ^ population > 2500000000) 
 
-# 9 // 
+# 9 // Show the name and population in millions and the GDP in billions for the countries of the continent 'South America'.
+SELECT name, ROUND(population/1000000,2), ROUND(gdp/1000000000,2)
+FROM world
+WHERE continent = 'South America'
+
 
 
 
